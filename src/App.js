54c0fb4f8e1c0eca4521c295ce3dca5dbb1ebc0f
Thiago <itsadeadh2@gmail.com>
2024-07-03 18:16:10 -0400

    Remove browserrouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import Terminal from './terminal';
 import Callback from './callback/callback'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css'; // Import App.css for the body styles
 import StoreProvider from './terminal/commandHistory/store'
 
