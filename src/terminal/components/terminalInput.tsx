b084e4674cb14fcd54f6d27b2065da9e79a42830
Thiago <itsadeadh2@gmail.com>
2024-07-15 15:28:15 -0400

    Add autofocus

--- src/terminal/components/terminalInput.tsx
+++ src/terminal/components/terminalInput.tsx
@@ -1,5 +1,4 @@
-// TerminalInput.tsx
-import React, { KeyboardEvent, RefObject } from 'react';
+import React, { useEffect, KeyboardEvent, RefObject } from 'react';
 
 interface TerminalInputProps {
     inputRef: RefObject<HTMLInputElement>;
@@ -8,21 +7,29 @@ interface TerminalInputProps {
     onKeyPress: (event: KeyboardEvent<HTMLInputElement>) => void;
 }
 
-const TerminalInput: React.FC<TerminalInputProps> = ({ inputRef, inputText, executing, onKeyPress }) => (
-    <>
-        <div>
-            &gt; {inputText}<span className="blinking-cursor"></span>
-        </div>
-        <input
-            type="text"
-            className="hidden-input"
-            ref={inputRef}
-            value=""
-            onKeyDown={onKeyPress}
-            onChange={() => {}} // Prevent React warning
-            disabled={executing}
-        />
-    </>
-);
+const TerminalInput: React.FC<TerminalInputProps> = ({ inputRef, inputText, executing, onKeyPress }) => {
+    useEffect(() => {
+        if (inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, [inputRef]);
+
+    return (
+        <>
+            <div>
+                &gt; {inputText}<span className="blinking-cursor"></span>
+            </div>
+            <input
+                type="text"
+                className="hidden-input"
+                ref={inputRef}
+                value=""
+                onKeyDown={onKeyPress}
+                onChange={() => {}} // Prevent React warning
+                disabled={executing}
+            />
+        </>
+    );
+};
 
 export default TerminalInput;
